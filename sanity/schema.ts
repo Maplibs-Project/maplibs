a25e714d6c2b40b7b2b8106d54eae08c5f144b10
Chirag Davé <c@chirag.io>
2024-05-27 10:11:30 -0400

    created sanity schema for Reflection

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,10 +1,7 @@
 import { type SchemaTypeDefinition } from 'sanity'
 
-import blockContent from './schemaTypes/blockContent'
-import category from './schemaTypes/category'
-import post from './schemaTypes/post'
-import author from './schemaTypes/author'
+import reflection from './schemaTypes/reflection'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [post, author, category, blockContent],
+  types: [reflection],
 }
---
+++ sanity/schemaTypes/reflection.ts
@@ -0,0 +1,51 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'reflection',
+  title: 'Reflection',
+  type: 'document',
+  fields: [
+
+    defineField({
+      name: 'neighborhood',
+      title: 'Neighborhood',
+      type: 'string',
+    }),
+    defineField({
+      name: 'place',
+      title: 'Place',
+      type: 'string',
+    }),
+    defineField({
+        name: 'sound',
+        title: 'Sound',
+        type: 'string',
+    }),
+    defineField({
+      name: 'smell',
+      title: 'Smell',
+      type: 'string',
+    }),
+    defineField({
+        name: 'event',
+        title: 'Event',
+        type: 'string',
+    }),
+    defineField({
+        name: 'street_corner_object',
+        title: 'Street Corner Object',
+        type: 'string',
+    }),
+    defineField({
+        name: 'first_name',
+        title: 'First Name',
+        type: 'string',
+    }),
+    defineField({
+        name: 'Email',
+        title: 'Email',
+        type: 'email',
+    }),
+  ],
+
+})
